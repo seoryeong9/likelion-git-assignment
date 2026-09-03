05e867f15429f50e12d0dbca2a59f18ad0bad863
0xwb7 <woobin200111@gmail.com>
2026-09-03 14:05:14 +0900

    feat: add attendance button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
+import AttendanceButton from "./AttendanceButton";
+
 function Header() {
-    return (
-      <header className="bg-neutral-900 px-8 py-5 text-white">
-        <div className="mx-auto flex max-w-4xl items-center justify-between">
+  return (
+    <header className="bg-neutral-900 px-8 py-5 text-white">
+      <div className="mx-auto flex max-w-4xl items-center justify-between">
+        <div>
           <h1 className="text-2xl font-bold">
             LIKELION Git Lab
           </h1>
-  
+
           <span className="text-sm text-neutral-400">
             Git Practice
           </span>
         </div>
-      </header>
-    );
-  }
-  
-  export default Header;
+
+        <AttendanceButton />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
