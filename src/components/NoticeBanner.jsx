51f3b52e1cae05fad15e9000e584a5511e22559b
seoryeong9 <seoryeong9.kim@gmail.com>
2026-09-11 23:37:55 +0900

    Revert "feat: modify notice"
    This reverts commit 95a68869c6d8833db4fa236e7a18be165ba43e81.

--- src/components/NoticeBanner.jsx
+++ src/components/NoticeBanner.jsx
@@ -1,7 +1,7 @@
 function NoticeBanner() {
     return (
-      <div className="rounded-xl bg-red-100 px-6 py-4 text-red-700">
-        메롱
+      <div className="rounded-xl bg-orange-100 px-6 py-4 text-orange-800">
+        멋사 프론트 화이팅
       </div>
     );
   }
